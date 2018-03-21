e9c3024dc5021b60a8e4d5e55e0f141835aa6569
Paul Cabrera <31601027+paulcabrera12@users.noreply.github.com>
2018-03-21 17:43:04 +0800

    Update Idea.js

--- models/Idea.js
+++ models/Idea.js
@@ -3,34 +3,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const IdeaSchema = new Schema({
-  title: {
+const UsersSchema = new Schema({
+  username: {
     type: String,
     required: true
   },
-  content: {
+    password: {
     type: String,
     required: true
-  },
-    genre: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-    rating: {
-    type: String,
-        default: 'Not Yet Rated'
-  },
-    isWatched: {
-    type: String,
-    default: false
   }
+
 });
 
 
 // Export ideas model
-module.exports = mongoose.model('ideas', IdeaSchema);
+module.exports = mongoose.model('users', UsersSchema);
 
