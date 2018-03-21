7dc297828388298d2ef4bbbdcd40418cf2d2994e
Paul Cabrera <31601027+paulcabrera12@users.noreply.github.com>
2018-03-21 17:42:30 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: 'mongodb://user:admin@ds115198.mlab.com:15198/movietodo' }
+  module.exports = { mongoURI: 'mongodb://admin:admin@ds145303.mlab.com:45303/users' }
 } else {
-  module.exports = { mongoURI: 'mongodb://user:admin@ds115198.mlab.com:15198/movietodo' }
+  module.exports = { mongoURI: 'mongodb://admin:admin@ds145303.mlab.com:45303/users' }
 }
